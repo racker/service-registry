4581d4b96dae8b029230209f261f2b1c7544d97c
Tomaz Muraus <tomaz@tomaz.me>
2013-05-20 17:20:20 -0700

    Use 127.0.0.1 instead of localhost and exit with the correct exit code in the create-schema script.

--- scripts/create-schema.sh
+++ scripts/create-schema.sh
@@ -21,9 +21,15 @@ make -C ./scripts/schema/ clean
 make -C ./scripts/schema/ setup
 
 sleep 0.5
-cqlsh localhost 9160 < ./scripts/schema/rproxy-combined.cql &
-cqlsh localhost 9160 < ./scripts/schema/farscape-combined.cql &
-wait
+
+cqlsh 127.0.0.1 9160 < ./scripts/schema/rproxy-combined.cql &
+cqlsh 127.0.0.1 9160 < ./scripts/schema/farscape-combined.cql &
+
+wait %1 %2
+
+exit_code=$?
+
 sleep 0.5
 
 echo "Done"
+exit $exit_code
